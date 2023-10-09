e58e73396c3aae35ba010ff48a65aab501939e4d
EduardoDEV08 <147440075+EduardoDEV08@users.noreply.github.com>
2023-10-09 16:24:40 -0300

    Opção de saída
    Foi adicionado uma 4 aba para o cartório, aba 4 opção de saída.

--- Cartorio_EBAC.c
+++ Cartorio_EBAC.c
@@ -134,6 +134,7 @@ int main()
 	   printf("\t1 - Registrar nomes\n");
 	   printf("\t2 - Consultar nomes\n");
 	   printf("\t3 - Deletar nomes\n");
+	   printf("\t4 - Sair do Sistema\n");
 	   printf("\n");
 	   printf("Opção: "); // Fim do menu
 	
@@ -154,6 +155,11 @@ int main()
     	    
     	    case 3:
     	    deletar();
+    	    break;
+    	    
+    	    case 4:
+    	    printf("Obrigado por utilizar o Sistema\n");
+    	    return 0;
     	    break;
     	    
     	    default:
